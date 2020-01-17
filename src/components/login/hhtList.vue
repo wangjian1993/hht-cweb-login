2231a89d4b12e01561ddd2a315ee07912c01902d
pepsi <550822730@qq.com>
2020-01-17 11:11:53 +0800

    修改设备列表显示

--- src/components/login/hhtList.vue
+++ src/components/login/hhtList.vue
@@ -1,67 +1,102 @@
 <template>
   <div>
     <div class="box">
-        <h1>hht管理系统</h1>
-        <div class="manBox">
-          <a style="background-color:#FF7676; box-shadow: 0px 2px 25px #FF7676" href="./f6s-manager/index.html">f6s资源管理系统</a>
-          <a style="background-color:#FFC17E; box-shadow: 0px 2px 25px #FFC17E" href="./hht-manager/index.html">j6管理系统</a>
-          <a style="background-color:#4CAEFF; box-shadow: 0px 2px 25px #4CAEFF" href="./thht-manager/index.html">hht测试管理系统</a>
-          <a style="background-color: rgb(255, 111, 165); box-shadow: 0px 2px 25px rgb(255, 111, 165)" href="./g63-manager/index.html">g63管理系统</a>
-          <a style="background-color: rgb(41, 41, 41); box-shadow: 0px 2px 25px rgb(41, 41, 41)" href="./g9s-manager/index.html">g9s管理系统</a>
-        </div>
+      <h1>hht管理系统</h1>
+      <div class="manBox">
+        <a v-for="(item,i) in devList" :key="i" :style="{ background: item.color }" @click="devTypeClick(item.type)">{{ item.name }}</a>
+      </div>
     </div>
-    <img src="../../assets/bg.png" alt="">
+    <img src="../../assets/bg.png" alt="" />
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      devList: [
+        {
+          name: 'f6s资源管理系统',
+          type: 1,
+          color: '#FF7676'
+        },
+        {
+          name: 'j6管理系统',
+          type: 2,
+          color: '#FFC17E'
+        },
+        {
+          name: 'hht测试管理系统',
+          type: 5,
+          color: '#4CAEFF'
+        },
+        {
+          name: 'g63管理系统',
+          type: 3,
+          color: 'rgb(255, 111, 165)'
+        },
+        {
+          name: 'g9s管理系统',
+          type: 4,
+          color: 'rgb(41, 41, 41)'
+        }
+      ]
+    }
+  },
+  methods: {
+    devTypeClick(type) {
+      localStorage.setItem('type', type)
+      window.location.href = './dist/index.html'
+    }
+  }
 }
 </script>
 
 <style>
-  * {
-    margin: 0;
-    padding: 0;
-  }
-  body {
-    background-color: #7CC1FF;
-  }
-  .box {
-    text-align: center;
-    width: 100%;
-    height: 40vh;
-    padding-top: 20vh;
-    background-color: #7CC1FF;
-  }
-  .manBox {
-    display: flex;
-    justify-content: space-around;
-  }
-  h1 {
-    font-size: 44px;
-    color: #fff;
-  }
-  .box a {
-    text-align: left;
-    display: inline-block;
-    text-decoration: none;
-    background:transparent;
-    color: #000;
-    width: 180px;
-    height: 60px;
-    line-height: 60px;
-    font-size: 15px;
-    color: #fff;
-    text-align: center;
-    border-radius: 4px;
-  }
-  img {
-    position: fixed;
-    bottom: 0px;
-    right: 0px;
-    height: 40vh;
-    width: 100%;
-  }
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: #7cc1ff;
+}
+.box {
+  text-align: center;
+  width: 100%;
+  height: 40vh;
+  padding-top: 20vh;
+  background-color: #7cc1ff;
+}
+.manBox {
+  display: flex;
+  justify-content: space-around;
+}
+.manBox a{
+  border-radius: 5vh;
+}
+h1 {
+  font-size: 44px;
+  color: #fff;
+}
+.box a {
+  text-align: left;
+  display: inline-block;
+  text-decoration: none;
+  background: transparent;
+  color: #000;
+  width: 180px;
+  height: 60px;
+  line-height: 60px;
+  font-size: 15px;
+  color: #fff;
+  text-align: center;
+  border-radius: 4px;
+}
+img {
+  position: fixed;
+  bottom: 0px;
+  right: 0px;
+  height: 40vh;
+  width: 100%;
+}
 </style>
